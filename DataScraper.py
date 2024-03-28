1d642028865ad8d8086016b5a82389383f8a6cfa
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-03-28 15:46:58 -0700

    pushed code Data Scraper

---
+++ DataScraper.py
@@ -0,0 +1,34 @@
+import requests
+import bs4
+from utility import clean_text
+from stopwords import custom_stopwords  # Import custom stop words
+
+
+
+def getWepageData(url):
+    
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = bs4.BeautifulSoup(response.text, "html.parser")
+            text = clean_text(soup.get_text())
+    
+    # Tokenize the text
+            words = text.split()
+
+    # Remove custom stop words
+            filtered_words = [word for word in words if word.lower() not in custom_stopwords]
+
+    # Join the filtered words back into a text
+            filtered_text = " ".join(filtered_words)
+
+        else:
+            print("Failed to retrieve the web page. Status code:", response.status_code)
+            return None
+    except Exception as e:
+        print("Error Encountered", e)
+        return None
+    return filtered_text        
+
+
