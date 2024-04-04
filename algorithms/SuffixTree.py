4cb2bc1e8fe862e01988e04b0662cd46f0a4a92e
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-03 18:14:38 -0700

    Removed Commented Code

--- algorithms/SuffixTree.py
+++ algorithms/SuffixTree.py
@@ -9,11 +9,3 @@ def count_pattern_occurrences(text, pattern):
 
     return len(occurrences)
 
-# # Sample text and pattern
-# text = "this is a sample text with multiple sample words and another sample."
-# pattern = "sample"
-
-# # Count occurrences of the pattern in the text
-# occurrence_count = count_pattern_occurrences(text, pattern)
-
-# print("Count of occurrences of the pattern:", occurrence_count)
